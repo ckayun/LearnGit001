890d4e99f7bb59c00d981244951d928a0022ea02
ckayun <dr.kayun@gmail.com>
2022-09-18 22:44:47 +0700

    correct by adding , at line 5

--- sample.py
+++ sample.py
@@ -2,4 +2,4 @@ from datetime import date, datetime
 print("Hello! current date and time is ", end="")
 print((datetime.now()).strftime("%A, %d %B %Y %H:%M:%S"))
 print("Hi ... ")
-print("After update it is now ... " (datetime.now()).strftime("%A, %d %B %Y %H:%M:%S"))
+print("After update it is now ... ", (datetime.now()).strftime("%A, %d %B %Y %H:%M:%S"))
